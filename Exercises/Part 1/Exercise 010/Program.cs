b4b810c5fd3abdd4eb0ae672edb1e7c70328e24d
Pia-N <pianurmi@hotmail.fi>
2020-10-21 11:58:22 +0300

    21.10.2020

--- Exercises/Part 1/Exercise 010/Program.cs
+++ Exercises/Part 1/Exercise 010/Program.cs
@@ -8,6 +8,18 @@ namespace exercise_10
         {
             // Write your code here:
 
+            Console.WriteLine("I will tell a story, but I need some information.\nGive a name for the main character:");
+
+            String name = Console.ReadLine();
+            Console.WriteLine("Give the character a profession:");
+            String title = Console.ReadLine();
+            Console.WriteLine("Here is the story:");
+            Console.WriteLine($"Once upon a time there was a {title} called {name}.");
+            Console.WriteLine($"On her/his way to work,{name} often pondered what being a {title} meant to them.");
+            Console.WriteLine($"When you work as a {title} you meet interesting people.");
+            Console.WriteLine($"{name} enjoys her/his work as a {title}.The end.");
+
+
         }
     }
 }
