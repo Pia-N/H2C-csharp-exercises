c7e63549bf58cceec0a645491d5660e6b6bb75b2
Pia-N <pianurmi@hotmail.fi>
2020-10-29 22:46:21 +0200

    29.10.2020

--- Exercises/Part 1/Exercise 015/Program.cs
+++ Exercises/Part 1/Exercise 015/Program.cs
@@ -8,6 +8,20 @@ namespace exercise_15
     {
 
       // Write your code here:
+      Console.WriteLine("Give a string:");
+      string userString = Console.ReadLine();
+      Console.WriteLine("Give an integer:");
+      int userInt = Convert.ToInt32(Console.ReadLine());
+      Console.WriteLine("Give a double:");
+      double userDouble = Convert.ToDouble(Console.ReadLine());
+      Console.WriteLine("Give a boolean:");
+      bool userBoolean = Convert.ToBoolean(Console.ReadLine());
+      Console.WriteLine("Your string:" + userString);
+      System.Console.WriteLine("Your integer:" + userInt);
+      System.Console.WriteLine("Your double:" + userDouble);
+      System.Console.WriteLine("Your boolean:" + userBoolean);
+
+
 
     }
   }
