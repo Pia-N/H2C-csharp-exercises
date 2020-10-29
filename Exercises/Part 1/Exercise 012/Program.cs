81f774b04b6840149ac6b3ebeb65e4f06afeab71
Pia-N <pianurmi@hotmail.fi>
2020-10-29 18:14:22 +0200

    29.10.2020

--- Exercises/Part 1/Exercise 012/Program.cs
+++ Exercises/Part 1/Exercise 012/Program.cs
@@ -7,6 +7,11 @@ namespace exercise_12
     public static void Main(string[] args)
     {
       // Write your code here:
+      Console.Writeline("Give a number!");
+      string userInput = Console.ReadLine();
+      int intValue = Convert.ToInt32(userInput);
+      Console.Writeline("You gave" + userInput);
+
 
     }
   }
