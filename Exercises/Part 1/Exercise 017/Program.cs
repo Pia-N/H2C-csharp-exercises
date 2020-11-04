4e1d5ee5268dc07a7c8eed28dd17198b271bbfb8
Pia-N <pianurmi@hotmail.fi>
2020-11-04 22:02:40 +0200

    4.11.2020

--- Exercises/Part 1/Exercise 017/Program.cs
+++ Exercises/Part 1/Exercise 017/Program.cs
@@ -8,6 +8,14 @@ namespace exercise_17
     {
 
       // Write your code here:
+      Console.WriteLine("Give the first number!");
+      int first = Convert.ToInt32(Console.ReadLine());
+      Console.WriteLine("Give the second number!");
+      int second = Convert.ToInt32(Console.ReadLine());
+      int sum = first + second;
+      Console.WriteLine("The sum is " + sum);
+
+
 
     }
   }
--- Exercises/Part 1/Exercise 018/Program.cs
+++ Exercises/Part 1/Exercise 018/Program.cs
@@ -8,6 +8,15 @@ namespace exercise_18
     {
 
       // Write your code here:
+      Console.WriteLine("Give the first number!");
+      int first = Convert.ToInt32(Console.ReadLine());
+      Console.WriteLine("Give the second number!");
+      int second = Convert.ToInt32(Console.ReadLine());
+      Console.WriteLine("Give the third number!");
+      int third = Convert.ToInt32(Console.ReadLine());
+      int sum = first + second + third;
+      Console.WriteLine("The sum is " + sum);
+
 
     }
   }
