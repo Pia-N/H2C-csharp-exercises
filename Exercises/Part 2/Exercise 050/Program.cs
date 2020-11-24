18a05fc5e036fa1492aa16b0cdec2dc5b34e3d1a
Pia-N <pianurmi@hotmail.fi>
2020-11-24 21:41:14 +0200

    24.11.2020

--- Exercises/Part 2/Exercise 050/Program.cs
+++ Exercises/Part 2/Exercise 050/Program.cs
@@ -7,12 +7,23 @@ namespace exercise_50
     public static void Main(string[] args)
     {
       // Call your method here:
+      Console.WriteLine("How many times?");
+      int userNumber = Convert.ToInt32(Console.ReadLine());
+      int numberOftimes = 0;
+
+      while(userNumber > numberOftimes)
+      {
+        PrintPhrase();
+        numberOftimes++;
+      }  
  
-    }
+    } // Write your method here:
+    public static void PrintPhrase() 
 
-    // Write your method here:
-    public static void PrintPhrase() {
-
+    {
+      Console.WriteLine("In a hole in the ground there lived a method");  
     }
+     
+    
   }
 }
