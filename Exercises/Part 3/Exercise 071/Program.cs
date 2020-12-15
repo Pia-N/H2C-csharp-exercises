05f96e1bcd4ec3b36f4df7b886b47716ac72beab
Pia-N <pianurmi@hotmail.fi>
2020-12-15 12:43:02 +0200

    15.12.2020

--- Exercises/Part 3/Exercise 071/Program.cs
+++ Exercises/Part 3/Exercise 071/Program.cs
@@ -8,28 +8,31 @@ namespace exercise_71
     public static void Main(string[] args)
     {
       List<int> list = new List<int>();
+      Console.WriteLine("Give a list of numbers(-1 stops). Then search for index");
+     
       while (true)
-      {
+        {
         int input = Convert.ToInt32(Console.ReadLine());
         if (input == -1)
         {
           break;
         }
         list.Add(input);
-      }
+        }
         Console.WriteLine("Search for?");
         int answer = Convert.ToInt32(Console.ReadLine());
-          int index = list.IndexOf(answer);
-          if (index != -1)
-          {
-            Console.WriteLine(answer +" is at index "+index);
-          }
-            else
-            {
-              Console.WriteLine("Number not found on given list.");
-            }
-
+        int index = list.IndexOf(answer);
         
+        for (int i = 0; i < list.Count; i++)
+        {
+          int number = list[i];
+        if (answer == number)
+        {
+          Console.WriteLine(answer +" is at index "+i);
+        }
+        }
+        Console.WriteLine("Number not found on given list.");
     }
   }
 }
+
